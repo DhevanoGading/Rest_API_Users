f512224d4dedd60a63c9fd721dbe4f180b2d2442
Dhevano Gading <dhevanogading@gmail.com>
2023-07-13 23:04:43 -0400

    add auth add karyawan

--- routes/karyawan.js
+++ routes/karyawan.js
@@ -3,7 +3,23 @@ const router = express.Router();
 const { validateToken } = require("../auth");
 const karyawanController = require("../controllers/karyawan");
 
-router.post("/", karyawanController.addKaryawan);
+/**
+ * @swagger
+ * /:
+ *  post:
+ *    summary: Add karywan
+ *    description: This api is used to add karyawan
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#components/schemas/Karyawan'
+ *    responses:
+ *      200:
+ *        description: Register successfully
+ */
+router.post("/", validateToken("admin"), karyawanController.addKaryawan);
 router.post("/login", karyawanController.login);
 router.post("/logout", karyawanController.logout);
 
