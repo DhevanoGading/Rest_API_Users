f9be7f90dd15cba68d09fa4708198e4509d15fdc
Dhevano Gading <dhevanogading@gmail.com>
2023-08-11 05:31:55 -0400

    membuat api dashboard sharing alokasi pmo, sdo di pmo, dan pmo for sdo

--- controllers/Dashboard/dashboardPmoController.js
+++ controllers/Dashboard/dashboardPmoController.js
@@ -1,9 +1,56 @@
-const { Op } = require("sequelize");
+const { Op, QueryTypes } = require("sequelize");
 const db = require("../../models");
 const Karyawan = db.Karyawan;
-const AssignmentKaryawan = db.AssignmentKaryawan;
-const Client = db.Client;
-const ProjectSetting = db.ProjectSetting;
+const {
+  AssignmentKaryawan,
+  Client,
+  ProjectSetting,
+  Board,
+} = require("../../models");
+
+queryResourceFromTo = async (from, to) => {
+  let query = `
+        SELECT
+            k.karyawanId,
+            MAX(k.namaLengkap) AS namaLengkap,
+            MAX(SUBSTRING(b.name, 1, 25)) AS name,
+            MAX(k.posisi) AS posisi,
+            MAX(clientId) AS clientId,
+            MAX(SUBSTRING(nama, 1, 25)) AS nama,
+            MAX(projectDiv) AS projectDiv,
+            COUNT(1) AS jumlah
+        FROM (
+            SELECT
+                XY.boardId,
+                XY.karyawanId,
+                ps.clientId,
+                c.nama,
+                ps.projectDiv
+            FROM (
+                SELECT DISTINCT
+                    boardId,
+                    karyawanId
+                FROM AssignmentKaryawans
+                WHERE UPPER(status) = 'ACTIVE'
+            ) XY,
+            ProjectSettings ps,
+            Clients c
+            WHERE ps.boardId = XY.boardId
+                AND ps.projectDiv = '${to}'
+                AND c.clientId = ps.clientId
+                AND UPPER(ps.status) = 'ACTIVE'
+        ) MMM,
+        Karyawans k,
+        Boards b
+        WHERE k.karyawanId = MMM.karyawanId
+            AND b.boardId = MMM.boardId
+            AND k.posisi != 'Quality Control'
+            AND k.divisi = '${from}'
+            AND k.resource = '${from}'
+            AND UPPER(k.status) = 'ACTIVE'
+        GROUP BY k.karyawanId;`;
+  return query;
+};
 
 module.exports = {
   async dasboardSummary(req, res) {
@@ -94,7 +141,7 @@ module.exports = {
       });
 
       res.status(200).json({
-        message: "Get Dasboard Summary Successfully!",
+        message: "Get Dasboard PMO Posisi Successfully!",
         result,
       });
     } catch (error) {
@@ -157,7 +204,7 @@ module.exports = {
       });
 
       res.status(200).json({
-        message: "Get Dasboard Summary Successfully!",
+        message: "Get Dasboard PMO Status Successfully!",
         result,
       });
     } catch (error) {
@@ -170,60 +217,95 @@ module.exports = {
   },
   async dashboardPmoSharing(req, res) {
     try {
-      const result = await Karyawan.findAll({
-        attributes: [
-          [
-            db.sequelize.literal(`CASE
-                        WHEN posisi = 'Developer' THEN 'Dev'
-                        WHEN posisi = 'Developer Analyst' THEN 'Dev An'
-                        WHEN posisi = 'Project Admin' THEN 'PA'
-                        WHEN posisi = 'Project Manager' THEN 'PM'
-                        WHEN posisi = 'System Administrator' THEN 'Sys Admin'
-                        WHEN posisi = 'System Analyst' THEN 'SA'
-                        WHEN posisi = 'Technical Writer' THEN 'TW'
-                        ELSE 'Other'
-                    END`),
-            "posisi",
-          ],
-          [
-            db.sequelize.fn(
-              "COUNT",
-              db.sequelize.literal(`CASE WHEN jumlah = 1 THEN 1 END`)
-            ),
-            "satu",
-          ],
-          [
-            db.sequelize.fn(
-              "COUNT",
-              db.sequelize.literal(`CASE WHEN jumlah = 2 THEN 1 END`)
-            ),
-            "double",
-          ],
-          [
-            db.sequelize.fn(
-              "COUNT",
-              db.sequelize.literal(`CASE WHEN jumlah > 2 THEN 1 END`)
-            ),
-            "morethan",
-          ],
-        ],
-        include: [
-          {
-            model: AssignmentKaryawan,
-            attributes: [],
-            where: {
-              [Op.substring]: "ACTIVE",
-            },
-            required: true,
-          },
-          {
-            model,
-          },
-        ],
-      });
+      const result = await db.sequelize.query(
+        `SELECT
+            CASE
+              WHEN posisi = 'Developer' THEN 'Dev'
+              WHEN posisi = 'Developer Analyst' THEN 'Dev An'
+              WHEN posisi = 'Project Admin' THEN 'PA'
+              WHEN posisi = 'Project Manager' THEN 'PM'
+              WHEN posisi = 'System Administrator' THEN 'Sys Admin'
+              WHEN posisi = 'System Analyst' THEN 'SA'
+              WHEN posisi = 'Technical Writer' THEN 'TW'
+              ELSE 'Other'
+            END AS 'posisi',
+            COUNT(CASE WHEN jumlah = 1 THEN 1 END) AS 'satu',
+            COUNT(CASE WHEN jumlah = 2 THEN 1 END) AS 'double',
+            COUNT(CASE WHEN jumlah > 2 THEN 1 END) AS 'morethan'
+          FROM (
+            SELECT *
+            FROM (
+              SELECT k.karyawanId, k.posisi, COUNT(1) jumlah
+              FROM (
+                SELECT XY.boardId, XY.karyawanId, ps.clientId, c.nama
+                FROM (
+                  SELECT DISTINCT boardId, karyawanId
+                  FROM AssignmentKaryawans
+                  WHERE UPPER(status) = 'ACTIVE'
+                ) XY, ProjectSettings ps, Clients c
+                WHERE ps.boardId = XY.boardId
+                  AND c.clientId = ps.clientId
+              ) MMM, Karyawans k
+              WHERE k.karyawanId = MMM.karyawanId
+                AND k.namaLengkap != 'Admin Trello'
+                AND k.posisi != 'Quality Control'
+                AND k.divisi = 'PMO'
+                AND k.resource = 'PMO'
+                AND UPPER(k.status) = 'ACTIVE'
+              GROUP BY karyawanId
+            ) NNN
+          ) XXXX
+          GROUP BY posisi
+          ORDER BY posisi ASC;`,
+        {
+          type: QueryTypes.SELECT,
+        }
+      );
 
       res.status(200).json({
-        message: "Get Dasboard Summary Successfully!",
+        message: "Get Dasboard PMO Sharing Successfully!",
+        result,
+      });
+    } catch (error) {
+      console.error("Error executing query:", error);
+      res.status(500).json({
+        message: "Error executing query",
+        error: error.message,
+      });
+    }
+  },
+  async dashboardSdoDiPmo(req, res) {
+    try {
+      const result = await db.sequelize.query(
+        await queryResourceFromTo("SDO", "PMO"),
+        {
+          type: QueryTypes.SELECT,
+        }
+      );
+
+      res.status(200).json({
+        message: "Get Dasboard SDO di PMO Successfully!",
+        result,
+      });
+    } catch (error) {
+      console.error("Error executing query:", error);
+      res.status(500).json({
+        message: "Error executing query",
+        error: error.message,
+      });
+    }
+  },
+  async dasboardPmoForSdo(req, res) {
+    try {
+      const result = await db.sequelize.query(
+        await queryResourceFromTo("PMO", "SDO"),
+        {
+          type: QueryTypes.SELECT,
+        }
+      );
+
+      res.status(200).json({
+        message: "Get Dasboard PMO di SDO Successfully!",
         result,
       });
     } catch (error) {
--- routes/dashboardPmoRoutes.js
+++ routes/dashboardPmoRoutes.js
@@ -6,5 +6,8 @@ const dashboardController = require('../controllers/Dashboard/dashboardPmoContro
 router.get("/", validateToken("admin"), dashboardController.dasboardSummary);
 router.get("/posisi", validateToken("admin"), dashboardController.dasboardPmoPosisi);
 router.get("/status", validateToken("admin"), dashboardController.dasboardPmoStatus);
+router.get("/sharing", validateToken("admin"), dashboardController.dashboardPmoSharing);
+router.get("/sdodipmo", validateToken("admin"), dashboardController.dashboardSdoDiPmo);
+router.get("/pmoforsdo", validateToken("admin"), dashboardController.dasboardPmoForSdo);
 
 module.exports = router;
